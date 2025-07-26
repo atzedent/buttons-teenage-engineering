a5500a41defc5280f93326a85ed87ba9a3d4fa4c
Matthias Hurrle <matthias.hurrle@imaverick.net>
2025-07-26 17:57:57 +0200

    Regelt die Statusindikatoren herunter

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 function init() {
-  const buttons = document.querySelectorAll("button");
-  buttons.forEach(button => {
-    button.addEventListener("click", () => {
-      buttons.forEach(btn => btn.dataset.active = "false");
-      button.dataset.active = "true";
-    });
-  });
+    const buttons = document.querySelectorAll("button")
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            if (button.dataset.active === "true") {
+                button.dataset.active = "false"
+                return
+            }
+            buttons.forEach(btn => btn.dataset.active = "false")
+            button.dataset.active = "true"
+        })
+    })
 }
 
 window.addEventListener("DOMContentLoaded", init)
