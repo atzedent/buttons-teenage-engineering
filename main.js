f6359b51a6e724a0a828843bc88a2df41e92a275
Matthias Hurrle <matthias.hurrle@imaverick.net>
2025-07-26 11:58:50 +0200

    Bringt Statusindikator

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log('Hello World!');
+function init() {
+  const buttons = document.querySelectorAll("button");
+  buttons.forEach(button => {
+    button.addEventListener("click", () => {
+      buttons.forEach(btn => btn.dataset.active = "false");
+      button.dataset.active = "true";
+    });
+  });
+}
+
+window.addEventListener("DOMContentLoaded", init)
